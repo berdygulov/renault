458e6dc0a4a843f2fb9ef876c3217eb210de6750
Tlegen Bayangali <tlegmusic@gmail.com>
2021-12-24 11:58:18 +0500

    applications/create.blade.php flash message added

--- app/Http/Controllers/Application/StoreController.php
+++ app/Http/Controllers/Application/StoreController.php
@@ -12,28 +12,26 @@ class StoreController extends Controller
     {
 
         $application = Application::create([
-            'client_surname' => $request->input('client_surname'),
-            'client_name' => $request->input('client_name'),
-            'client_patronymic' => $request->input('client_patronymic'),
-            'client_phone' => $request->input('client_phone'),
-            'additional_phone' => $request->input('additional_phone'),
-            'car_brand' => $request->input('car_brand'),
-            'car_model' => $request->input('car_model'),
-            'car_production_year' => $request->input('car_production_year'),
-            'car_vin_number' => $request->input('car_vin_number'),
-            'date_time' => $request->input('date_time'),
+            'client_surname'       => $request->input('client_surname'),
+            'client_name'          => $request->input('client_name'),
+            'client_patronymic'    => $request->input('client_patronymic'),
+            'client_phone'         => $request->input('client_phone'),
+            'additional_phone'     => $request->input('additional_phone'),
+            'car_brand'            => $request->input('car_brand'),
+            'car_model'            => $request->input('car_model'),
+            'car_production_year'  => $request->input('car_production_year'),
+            'car_vin_number'       => $request->input('car_vin_number'),
+            'date_time'            => $request->input('date_time'),
             'approximate_duration' => $request->input('approximate_duration'),
-            'description' => $request->input('description'),
-            'master_id' => $request->input('master_id'),
-            'status' => 'waiting',
-            'user_id' => auth()->id(),
+            'description'          => $request->input('description'),
+            'master_id'            => $request->input('master_id'),
+            'status'               => 'waiting',
+            'user_id'              => auth()->id(),
         ]);
 
         $application->services()->attach($request->services);
 
-
         return back()
-            ->with('application.create.success', 'Заявка успешно создана!');
-
+            ->with('crud_success', 'Заявка успешно создана!');
     }
 }
--- resources/views/backend/applications/create.blade.php
+++ resources/views/backend/applications/create.blade.php
@@ -6,6 +6,12 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
+                @if(session()->has('crud_success'))
+                    <div class="alert alert-success alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                        <h5><i class="icon fas fa-check"></i> {{ session('crud_success') }}</h5>
+                    </div>
+                @endif
                 <div class="card card-info">
                     <div class="card-header">
                         <h3 class="card-title">Новая заявка</h3>
